7b0eb105fe7e1d8f1071a7d5bed13bbe94f68263
legarrid <60114371+legarrid@users.noreply.github.com>
2020-01-22 23:15:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("What is your first name?");
+var color = prompt("What is your favorite color?")
+
+document.body.innerHTML = "My first name is "+name+" and my favorite color is "+color+" bty I love you <3;"
